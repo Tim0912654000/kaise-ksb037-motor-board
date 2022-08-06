d9ec41cfa62063a361f2fc3aef10979f83497e88
Tim0912654000 <110079041+Tim0912654000@users.noreply.github.com>
2022-08-06 23:51:26 +0800

    增加功能

--- main.ts
+++ main.ts
@@ -1,17 +1,28 @@
+function doMotor12STOP () {
+    pins.analogWritePin(AnalogPin.P1, 0)
+}
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Yes)
+    doMotor12()
+    strip = neopixel.create(DigitalPin.P16, 8, NeoPixelMode.RGB)
+    strip.showColor(neopixel.colors(NeoPixelColors.White))
+})
+function doMotor12 () {
     pins.analogWritePin(AnalogPin.P1, 586)
     pins.digitalWritePin(DigitalPin.P8, 1)
-})
+}
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.No)
-    pins.analogWritePin(AnalogPin.P1, 0)
+    doMotor12STOP()
 })
+let light2 = 0
+let strip: neopixel.Strip = null
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-    basic.showString("O")
-    basic.showString("" + (Math.round(dstemp.celsius(DigitalPin.P0))))
-    basic.pause(1000)
-    basic.showString("i")
-    basic.showString("" + (input.temperature()))
+    basic.showString("" + (Math.round(dstemp.celsius(DigitalPin.P15))))
+    basic.showString("/")
+    basic.showString("" + (Math.round(input.temperature() - 1.5)))
+    basic.showString("/")
+    light2 = pins.analogReadPin(AnalogPin.P0)
+    basic.showString("" + (light2))
 })
